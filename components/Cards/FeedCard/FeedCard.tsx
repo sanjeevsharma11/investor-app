489abff96823a853798bbeb0235e94f0e22c903f
webcoderspeed <webcoderspeed@gmail.com>
2022-10-29 20:16:20 +0530

    feed card instrument info wrapper

--- components/Cards/FeedCard/FeedCard.tsx
+++ components/Cards/FeedCard/FeedCard.tsx
@@ -1,74 +1,28 @@
-import { View, Text, TouchableOpacity, Image } from "react-native";
-import React from "react";
-import { IFeed } from "../../../store/types/feed.types";
-import { StyledComponent } from "nativewind";
-import ExpertReturnIcon from "../../svg/feedcard/ExpertReturnIcon";
-import ExpertAccuracyIcon from "../../svg/feedcard/ExpertAccuracyIcon";
+import { View, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { IFeed } from '../../../store/types/feed.types';
+import { StyledComponent } from 'nativewind';
+import ExpertInfoWrapper from './ExpertInfoWrapper';
+import InstrumentInfoWrapper from './InstrumentInfoWrapper';
 
 const FeedCard = ({ feed }: { feed: IFeed }) => {
   return (
     <StyledComponent
       component={TouchableOpacity}
-      className="p-4 bg-white rounded-md shadow-sm my-2"
+      className='p-4 bg-white rounded-md shadow-sm my-2'
       activeOpacity={1}
     >
       <ExpertInfoWrapper expert={feed.expert} />
-      <StyledComponent component={View} className="mt-4">
-        <StyledComponent component={Text} className="text-lg font-bold">
-          {feed.instrumentName}
-        </StyledComponent>
-      </StyledComponent>
+      <InstrumentInfoWrapper
+        instrumentName={feed.instrumentName}
+        transactionType={feed.transactionType}
+        LTP={feed.LTP}
+        instrumentReturn={feed.instrumentReturn}
+        premiumTradeType={feed.premiumTradeType}
+        bullishOrBearish={feed.bullishOrBearish}
+      />
     </StyledComponent>
   );
 };
 
 export default FeedCard;
-
-const ExpertInfoWrapper = ({ expert }: { expert: IFeed["expert"] }) => {
-  return (
-    <StyledComponent component={View} className="flex-row justify-between">
-      <StyledComponent
-        component={View}
-        className="flex-row items-center justify-center gap-2"
-      >
-        <StyledComponent
-          component={Image}
-          className="w-[29px] h-[29px] rounded-full border-2 border-gray-200"
-          source={{ uri: expert.avatar }}
-        />
-
-        <StyledComponent
-          component={View}
-          className="flex-row items-center space-x-1"
-        >
-          <StyledComponent
-            component={Text}
-            className="font-bold text-[12px] text-[#0d0c0c]"
-          >
-            {expert.name}
-          </StyledComponent>
-
-          <StyledComponent
-            component={View}
-            className="bg-[#023047] py-1 px-1 flex-row items-center gap-x-1 rounded-full h-5"
-          >
-            <ExpertAccuracyIcon />
-            <StyledComponent component={Text} className="text-white text-[8px]">
-              {expert.return}%
-            </StyledComponent>
-          </StyledComponent>
-
-          <StyledComponent
-            component={View}
-            className="bg-[#023047] py-1 px-1 flex-row items-center gap-x-1 rounded-full h-5"
-          >
-            <ExpertReturnIcon />
-            <StyledComponent component={Text} className="text-white text-[8px]">
-              {expert.return}%
-            </StyledComponent>
-          </StyledComponent>
-        </StyledComponent>
-      </StyledComponent>
-    </StyledComponent>
-  );
-};
---
+++ components/Cards/FeedCard/InstrumentInfoWrapper.tsx
@@ -0,0 +1,162 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { StyledComponent } from 'nativewind';
+import { IFeed } from '../../../store/types/feed.types';
+import StartIcon from '../../svg/feedcard/StartIcon';
+
+interface IInstrumentInfoWrapper {
+  instrumentName: IFeed['instrumentName'];
+  transactionType: IFeed['transactionType'];
+  LTP: IFeed['LTP'];
+  instrumentReturn: IFeed['instrumentReturn'];
+  premiumTradeType: IFeed['premiumTradeType'];
+  bullishOrBearish: IFeed['bullishOrBearish'];
+}
+
+const InstrumentInfoWrapper = ({
+  instrumentName,
+  transactionType,
+  LTP,
+  instrumentReturn,
+  premiumTradeType,
+  bullishOrBearish,
+}: IInstrumentInfoWrapper) => {
+  return (
+    <StyledComponent
+      component={View}
+      className='flex-row items-center mt-2'
+    >
+      <BuyOrSellIcon
+        transactionType={transactionType}
+        premiumTradeType={premiumTradeType}
+      />
+      <InstrumentName
+        instrumentName={instrumentName}
+        LTP={LTP}
+      />
+      <SinceAdded
+        instrumentReturn={instrumentReturn}
+        bullishOrBearish={bullishOrBearish}
+      />
+    </StyledComponent>
+  );
+};
+
+export default InstrumentInfoWrapper;
+
+const BuyOrSellIcon = ({
+  transactionType,
+  premiumTradeType,
+}: {
+  transactionType: IFeed['transactionType'];
+  premiumTradeType: IFeed['premiumTradeType'];
+}) => {
+  return (
+    <StyledComponent
+      component={View}
+      className={
+        transactionType === 'BUY'
+          ? 'items-center justify-center rounded-md w-8 h-8'
+          : 'items-center justify-center rounded-md w-8 h-8'
+      }
+      style={
+        premiumTradeType === 'PAID'
+          ? {
+              backgroundColor: 'rgba(255, 226, 183, 1)',
+            }
+          : transactionType === 'BUY'
+          ? {
+              backgroundColor: 'rgba(202, 255, 217, 1)',
+            }
+          : {
+              backgroundColor: 'rgba(255, 72, 72, 0.15)',
+            }
+      }
+    >
+      {premiumTradeType === 'PAID' ? (
+        <StyledComponent
+          component={Text}
+          className={
+            transactionType === 'BUY' ? 'text-[#0D9F36]' : 'text-[#FF4848]'
+          }
+        >
+          <StartIcon />
+        </StyledComponent>
+      ) : (
+        <StyledComponent
+          component={Text}
+          className={
+            transactionType === 'BUY' ? 'text-[#0D9F36]' : 'text-[#FF4848]'
+          }
+        >
+          {transactionType === 'BUY' ? 'B' : 'S'}
+        </StyledComponent>
+      )}
+    </StyledComponent>
+  );
+};
+
+const InstrumentName = ({
+  instrumentName,
+  LTP,
+}: {
+  instrumentName: IFeed['instrumentName'];
+  LTP: IFeed['LTP'];
+}) => {
+  return (
+    <StyledComponent
+      component={Text}
+      className='text-black font-medium ml-2 text-base'
+    >
+      {instrumentName}{' '}
+      <StyledComponent
+        component={Text}
+        className='text-gray-400 font-normal text-sm'
+      >
+        {LTP}
+      </StyledComponent>
+    </StyledComponent>
+  );
+};
+
+const SinceAdded = ({
+  instrumentReturn,
+  bullishOrBearish,
+}: {
+  instrumentReturn: IFeed['instrumentReturn'];
+  bullishOrBearish: IFeed['bullishOrBearish'];
+}) => {
+  const className = `
+  text-gray-400 font-normal text-[10px]
+  ${
+    instrumentReturn > 0 && bullishOrBearish.toLowerCase() === 'bullish'
+      ? 'text-[#0D9F36]'
+      : instrumentReturn > 0 && bullishOrBearish.toLowerCase() !== 'bullish'
+      ? 'text-[#FF4848]'
+      : instrumentReturn < 0 && bullishOrBearish.toLowerCase() === 'bullish'
+      ? 'text-[#FF4848]'
+      : instrumentReturn < 0 && bullishOrBearish.toLowerCase() !== 'bullish'
+      ? 'text-[#0D9F36]'
+      : ''
+  }`;
+
+  return (
+    <StyledComponent
+      component={View}
+      className='ml-2'
+    >
+      <StyledComponent
+        component={Text}
+        className={className}
+      >
+        {instrumentReturn}%
+      </StyledComponent>
+      <StyledComponent
+        component={Text}
+        className='text-[#b5b5b5] text-[6px] mt-[-2px]'
+      >
+        Since Added
+      </StyledComponent>
+    </StyledComponent>
+  );
+};
